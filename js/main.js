36286c2cd8d6d592d56f1f349d53774c3f3a42c6
Callum <b6010572@my.shu.ac.uk>
2020-02-21 14:50:09 +0000

    Load creatures.json data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$.onload(function() {
+$(document).ready(function () {
+	$.getJSON('creatures.json', function(data) {
+		var creatures = [];
 
+		$.each(data, function(key, creature) {
+			if("name" in creature) {
+				creatures.push('<li id="' + key + '">' + creature["name"] + "</li>")
+			}
+		});
+
+		$( "<ul/>", {
+			"class": "my-new-list",
+			html: creatures.join( "" )
+		}).appendTo( "body" );
+	})
 });
