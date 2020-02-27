345ce3c93c96a30d958bced54675a79f07d8385a
Callum <b6010572@my.shu.ac.uk>
2020-02-27 11:57:06 +0000

    Add removing creatures from the list

--- js/main.js
+++ js/main.js
@@ -10,28 +10,35 @@ $(document).ready(function () {
 
 		$.each(data, function(key, creature) {
 			if("name" in creature) {
-				$('#library>ul').append('<li onclick="addCreature(' + key + ')" id="' + key + '">' + creature["name"] + '</li>');
+				$('#library>ul').append('<li onclick="addCreature(' + key + ')" data-id="' + key + '">' + creature["name"] + '</li>');
 			}
 		});
 	})
 });
 
 function addCreature(key) {
+	var initiativeListBody = $('#initiative-list>table>tbody');
+
 	initiativeList.push(creatures[key]);
-	$('#initiative-list>table>tbody').append(
-		'<tr>' +
-			'<td>' + 
-				0 +
-			'</td>' +
-			'<td>' + 
-				creatures[key]["name"] +
-			'</td>' +
-			'<td>' + 
-				creatures[key]["hit_points"] +
-			'</td>' +
-			'<td>' + 
-				creatures[key]["armor_class"] +
-			'</td>' +
-		'</tr>'
-	);
+
+	var html = '<tr data-id="' + (initiativeList.length - 1) + '"" data-key="' + key + '">' +
+		'<td>' +
+			(initiativeList.length - 1) +
+		'</td>' +
+		'<td>' +
+			creatures[key]["name"] +
+		'</td>' +
+		'<td>' + 
+			creatures[key]["hit_points"] +
+		'</td>' +
+		'<td>' + 
+			creatures[key]["armor_class"] +
+		'</td>' +
+	'</tr>';
+
+	// Add the markup into the DOM and associate an anonymous event with it
+	var newListing = $('#initiative-list>table>tbody').append(html);
+	newListing.click(function () {
+		$(this).children().remove()
+	});
 }
