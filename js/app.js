3c85ddfadce6bfda5311677146138cd5cf073cd3
Callum <b6010572@my.shu.ac.uk>
2020-03-24 11:39:37 +0000

    Add support for duplicate creatures

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ var app = new Vue({
 				<table> 
                     <thead>
                         <tr>
+                        	<th>Index</th>
                             <th>Initiative</th>
                             <th>Name</th>
                             <th>Hit Points</th>
@@ -30,8 +31,9 @@ var app = new Vue({
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for='combatant in data'>
-                            <td>?</td>
+                        <tr v-for='(index, combatant) in data'>
+                            <td>{{ index }}</td>
+                            <td>???</td>
                             <td>{{ combatant.name }}</td>
                             <td>{{ combatant.hit_points }}</td>
                             <td>{{ combatant.armor_class }}</td>
@@ -43,7 +45,10 @@ var app = new Vue({
 	},
 	methods: {
 		addCreature: function (index) {
-			this.initiativeOrder.push(this.creatures[index]);
+			var nextCombatant = JSON.parse(JSON.stringify(this.creatures[index]));
+
+			// Add this creature to the initiative order
+			this.initiativeOrder.push(nextCombatant);
 		}
 	},
 	created: function () {
