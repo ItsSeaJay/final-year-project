1f79c72d6f826c145842e4598a23a186d05a30e9
Callum <b6010572@my.shu.ac.uk>
2020-03-24 13:26:57 +0000

    Make app responsive to state and attempt to alter rolled initiative scores

--- js/app.js
+++ js/app.js
@@ -4,18 +4,25 @@ Vue.component(
 		props: [
 			'creature'
 		],
-		template: '<li>{{ index }} {{ creature.name }}</li>'
+		template: '<li>{{ creature.name }}</li>'
 	}
 );
+
+const states = {
+	normal: 0,
+	precombat: 1,
+	combat: 2
+}
 
 var app = new Vue({
 	el: '#app',
 	data: {
+		state: states.normal,
 		creatures: [],
 		initiativeOrder: []
 	},
 	components: {
-		initiative: {
+		'initiative-order': {
 			props: [
 				'data'
 			],
@@ -33,7 +40,7 @@ var app = new Vue({
                     <tbody>
                         <tr v-for='(index, combatant) in data'>
                             <td>{{ index }}</td>
-                            <td>???</td>
+                            <td>{{ combatant.initiative_score }}</td>
                             <td>{{ combatant.name }}</td>
                             <td>{{ combatant.hit_points }}</td>
                             <td>{{ combatant.armor_class }}</td>
@@ -41,14 +48,67 @@ var app = new Vue({
                     </tbody>
                 </table>
 			`
+		},
+		'initiative-modal': {
+			props: [
+				'data'
+			],
+			template: `
+				<div class="modal">
+					<ul v-for="(index, combatant) in data">
+						<li>
+							<span>{{ index }} - {{ combatant.name }}</span>
+							<input type="number" value="{{ combatant.initiative_score }}" v-on:input="updateInitiative(index, $value)">
+						</li>
+					</ul>
+					<button type="button">Cancel</button>
+					<button type="button" v-on:click="beginCombat">Confirm</button>
+				</div>
+			`
 		}
 	},
 	methods: {
+		rollInitiative: function(creature) {
+			// Calculate the initiative bonus based on the target's
+			// dexterity score
+			var initiativeBonus = Math.floor((this.initiativeOrder[combatant].dexterity - 10) / 2);
+			// Roll the initiative score for this creature based on the bonus
+			var initiativeScore = Math.round(Math.random() * 20) + initiativeBonus;
+		
+			creature.initiative_score = initiativeScore;
+		},
+		updateInitiative: function (index, value) {
+			this.initiativeOrder[index].initiative_score = value;
+		},
 		addCreature: function (index) {
 			var nextCombatant = JSON.parse(JSON.stringify(this.creatures[index]));
 
+			if (this.state === states.precombat) {
+				this.rollInitiative(nextCombatant);
+			}
+
 			// Add this creature to the initiative order
 			this.initiativeOrder.push(nextCombatant);
+		},
+		setupEncounter: function (event) {
+			if (this.initiativeOrder.length < 2) {
+				alert('Need at least two combatants per encounter!');
+
+				return;
+			}
+
+			for (combatant in this.initiativeOrder) {
+				this.rollInitiative(this.initiativeOrder[combatant]);
+			}
+
+			this.state = states.precombat;
+		},
+		cancelEncounter: function (event) {
+
+		},
+		beginCombat: function (event) {
+			this.state = states.combat;
+			console.log(this.state);
 		}
 	},
 	created: function () {
